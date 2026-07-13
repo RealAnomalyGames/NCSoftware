0263219831c6dc3938db7d51e90dcbf2194b006d
RealAnomalyGames <146598908+RealAnomalyGames@users.noreply.github.com>
2026-07-13 02:06:58 +0000

    Build 001 Step 4

--- engine/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-console.log("NC Software Engine");
-console.log("Version 0.01 Build 001");
-const app = document.getElementById("app");
-if (app) {
-    app.textContent = "NC Software Engine Initialized";
-}
-//# sourceMappingURL=main.js.map
